02a70aa8a47ffa7839d2242e104d62989fe54b61
Jeremy Winterberg <winterjd@uwec.edu>
2015-06-10 14:31:06 -0500

    add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -81,9 +81,27 @@ class UserTest < ActiveSupport::TestCase
         assert_not jeremy.following?(archer)
         jeremy.follow(archer)
         assert jeremy.following?(archer)
-        assert archer.follwers.include?(jeremy)
+        assert archer.followers.include?(jeremy)
         jeremy.unfollow(archer)
         assert_not jeremy.following?(archer)
+    end
+
+    test "feed should have the right posts" do
+        jeremy = users(:jeremy)
+        archer = users(:archer)
+        lana   = users(:lana)
+        # Posts from followed user
+        lana.microposts.each do |post_following|
+            assert jeremy.feed.include?(post_following)
+        end
+        # Posts from self
+        jeremy.microposts.each do |post_self|
+            assert jeremy.feed.include?(post_self)
+        end
+        # Posts from unfollowed user
+        archer.microposts.each do |post_unfollowed|
+            assert_not jeremy.feed.include?(post_unfollowed)
+        end
     end
 
 end
